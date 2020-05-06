3abab7d5a4eb82c2acf1bab832d2e11fc49b0f0d
Anar <anar.rzayev94@gmail.com>
2020-05-06 16:06:56 +0400

    connect auth and greeting services via web server

--- web_server.go
+++ web_server.go
@@ -7,37 +7,62 @@ import (
 	"github.com/micro/go-micro"
 	"log"
 	"net/http"
-	pb "github.com/Anarr/gomicrodev/proto/auth"
+	au "github.com/Anarr/gomicrodev/proto/auth"
+	gr "github.com/Anarr/gomicrodev/proto/greeter"
 )
+
+var authService, greetingService micro.Service
+
 var listenAddr = ":8080"
 type User struct {
 	ID int
 	Name string
 }
+
+func init () {
+	authService = micro.NewService(micro.Name("auth"))
+	greetingService = micro.NewService(micro.Name("greeter"))
+}
+
 func loginHandler(writer http.ResponseWriter, req *http.Request) {
-	var s micro.Service
 	email := req.URL.Query().Get("email")
 	pass := req.URL.Query().Get("pass")
 
-	client := pb.NewAuthServiceClient("auth", s.Client())
-	r := &bp.AuthRequest{
+	client := au.NewAuthServiceClient("auth", authService.Client())
+	r := &au.AuthRequest{
 		Email: email,
 		Password:pass,
 	}
 	res, err := client.Login(context.Background(), r)
+	writer.Header().Add("Content-type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	if err != nil {
+		fmt.Fprint(writer, err)
+		return
+	}
+
+	js, _ := json.Marshal(&res)
+	writer.Write(js)
+}
+func greetingHandler(writer http.ResponseWriter, req *http.Request) {
+	client := gr.NewGreeterClient("greeter", greetingService.Client())
+	res, err := client.Hello(context.Background(), &gr.Request{Name: "Anar"})
+	writer.Header().Add("Content-type", "application/json")
+	writer.WriteHeader(http.StatusOK)
 
 	if err != nil {
 		fmt.Fprint(writer, err)
+		return
 	}
 
-	writer.Header().Add("Content-type", "application/json")
-	writer.WriteHeader(http.StatusOK)
-	js, _ := json.Marshal(&res)
-	writer.Write(js)
+	greeting, _ := json.Marshal(res)
+	writer.Write(greeting)
+	return
 }
 
 func main() {
 	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/greeting", greetingHandler)
 	if err := http.ListenAndServe(listenAddr, nil); err !=nil {
 		log.Fatal("Web server down")
 	}
